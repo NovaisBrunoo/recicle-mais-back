810cadd08e2f233ddf965950a55b502f49acaa5f
isabeldiana <isabel_diana@outlook.com>
2024-03-05 11:39:29 -0300

    add: Email validation

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,19 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { UserDTO } from './user.dto';
 import * as bcrypt from 'bcrypt';
-
+import { isEmail } from 'class-validator';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService){}
   async create(data: UserDTO){
-    const userExist = await this.prisma.user.findUnique({
+    if (!isEmail(data.email)) {
+      throw new ConflictException({message:"Formato de e-mail inválido."});
+    }
+    const userExist = await this.prisma.user.findFirst({
       where: {
         email: data.email,
       }
     }) 
     if(userExist){
-      throw new Error("Este email já esta sendo utilizado.")
+      throw new ConflictException({message:"Este email já esta sendo utilizado."})
     }
     const saltOrRounds = 10;
     const hash = await bcrypt.hash(data.password, saltOrRounds);
