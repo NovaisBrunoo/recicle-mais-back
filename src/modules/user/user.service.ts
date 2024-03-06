bb2a491da26035e6cdf067b2fda4e9bed4b97c1c
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-03-06 10:04:52 -0300

    Td rodando

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,27 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { UserDTO } from './user.dto';
 import * as bcrypt from 'bcrypt';
-
+import { isEmail } from 'class-validator';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
-
   async create(data: UserDTO) {
-    const userExist = await this.prisma.user.findUnique({
+    if (!isEmail(data.email)) {
+      throw new ConflictException({ message: 'Formato de e-mail inválido.' });
+    }
+    const userExist = await this.prisma.user.findFirst({
       where: {
-        email: data.email as string,
+        email: data.email,
       },
     });
     if (userExist) {
-      throw new Error('Este email já esta sendo utilizado.');
+      throw new ConflictException({
+        message: 'Este email já esta sendo utilizado.',
+      });
     }
     const saltOrRounds = 10;
     const hash = await bcrypt.hash(data.password, saltOrRounds);
     const user = await this.prisma.user.create({
       data: {
         ...data,
-        password: hash as string,
+        password: hash,
       },
     });
     return user;
